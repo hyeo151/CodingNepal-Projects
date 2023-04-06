cb3306d9a859a00f63df66a8412f8c03ef5b29ad
heongi <khyeo251@gmail.com>
2023-04-06 19:33:59 +1200

    worked on javascript

--- 5_TypingSpeedTestGame/script.js
+++ 5_TypingSpeedTestGame/script.js
@@ -1,18 +1,32 @@
 textEle = document.querySelector(".text-view");
 inputEle = document.querySelector("input");
 
-let inputText = "";
+let texts;
 
 const init = () => {
-    const texts = paragraphs[Math.floor(Math.random()*paragraphs.length)];
-    textEle.innerHTML = texts;
+    texts = paragraphs[Math.floor(Math.random()*paragraphs.length)];
+    textEle.innerHTML = `<span class="next-letter">${texts.slice(0,1)}</span>${texts.slice(1,texts.length)}`;
 }
 
-const checkText = (e) => {
-    inputText += e.key;
-    console.log(inputText);
+const checkText = () => {
+
+    let inputText = inputEle.value;
+
+    htmlTag = `<span class="correct-letter">`
+    for (let i = 0; i < inputText.length; i++) {
+        
+        if(texts[i] !== inputText[i]){
+            console.log("incorrect");
+            htmlTag += `<span class="incorrect-letter">${texts[i]}</span>`
+        }else{
+            htmlTag += texts[i];
+        }
+    }
+    htmlTag += `</span><span class="next-letter">${texts.slice(inputText.length,inputText.length+1)}</span>${texts.slice(inputText.length+1,texts.length)}`
+    textEle.innerHTML = htmlTag;
+
 }
 
 init();
-document.addEventListener("focus",()=>{console.log("focused")});
-// inputEle.addEventListener("keydown",checkText)
+document.addEventListener("click",()=>{inputEle.focus();});
+inputEle.addEventListener("input",checkText);
